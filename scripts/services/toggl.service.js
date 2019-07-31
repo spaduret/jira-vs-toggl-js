62cd0106fed2b076cab6c899630f97850ba09fdc
Serghei Paduret <serghei.paduret@monsato.md>
2019-07-31 15:21:21 +0300

    fix issue when extensions installed first time

--- scripts/services/toggl.service.js
+++ scripts/services/toggl.service.js
@@ -3,50 +3,59 @@ define([
     'underscore',
     'moment',
     'settings'
-], function ($, _, moment, settings) {
+], function($, _, moment, settings) {
     'use strict';
 
     const taskPattern = /[A-Z]+-\d+/;
-    let _workspaceId = null,
-        _userId = null;
 
-    const authToken = btoa(settings.toggl.apiToken + ':api_token');
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + authToken
-    };
     const urls = {
         me: 'https://www.toggl.com/api/v8/me',
         summary: 'https://toggl.com/reports/api/v2/summary',
         details: 'https://toggl.com/reports/api/v2/details'
     };
 
-    const settingsXhr = $.ajax({
-        url: urls.me,
-        headers: headers,
-        type: 'GET',
-        async: true
-    }).then(function (response) {
-        const workspace = _(response.data.workspaces)
-            .find(function (ws) {
-                return ws.name === settings.toggl.workspace;
-            });
+    let headers = {};
+    const deferred = new $.Deferred();
+    const getSettingsAsync = function() {
+        if(deferred.state() != "resolved") {
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(settings.toggl.apiToken + ':api_token')
+            };
 
-        if (workspace) {
-            _workspaceId = workspace.id;
-            _userId = response.data.id;
+            $
+                .ajax({
+                    url: urls.me,
+                    headers: headers,
+                    type: 'GET',
+                    async: true
+                })
+                .fail(deferred.reject)
+                .then(function(response) {
+                    const workspace = _(response.data.workspaces)
+                        .find(function(ws) {
+                            return ws.name === settings.toggl.workspace;
+                        });
+
+                    deferred.resolve({
+                        workspaceId: workspace.id,
+                        userId: response.data.id
+                    });
+                });
         }
-    });
+
+        return deferred.promise();
+    };
 
     return {
         settings: settings.toggl,
-        getSummaryAsync: function (period) {
-            return settingsXhr
-                .then(() => {
+        getSummaryAsync: function(period) {
+            return getSettingsAsync()
+                .then((settings) => {
                     const params = {
                         user_agent: 'jira',
-                        workspace_id: _workspaceId,
-                        user_ids: _userId,
+                        workspace_id: settings.workspaceId,
+                        user_ids: settings.userId,
                         since: period.start.format('YYYY-MM-DD'),
                         until: period.end.format('YYYY-MM-DD')
                     };
@@ -57,14 +66,14 @@ define([
                         type: 'GET',
                         async: true
                     })
-                        .then(function (response) {
+                        .then(function(response) {
                             return _.chain(response.data)
                                 .pluck('items')
                                 .flatten(true)
-                                .filter(function (item) {
+                                .filter(function(item) {
                                     return taskPattern.test(item.title.time_entry);
                                 })
-                                .map(function (item) {
+                                .map(function(item) {
                                     return {
                                         time: item.time,
                                         title: item.title.time_entry,
@@ -72,11 +81,11 @@ define([
                                     };
                                 })
                                 .groupBy('task')
-                                .map(function (group, task) {
+                                .map(function(group, task) {
                                     return {
                                         task: task,
                                         title: group[0].title,
-                                        time: Math.round(_(group).reduce(function (memo, item) {
+                                        time: Math.round(_(group).reduce(function(memo, item) {
                                             return memo + item.time;
                                         }, 0) / 1000)
                                     };
@@ -86,13 +95,13 @@ define([
                 });
         },
         // get all time entries for the last 365 days
-        getTotalLoggedTimeByTitleAsync: function (title) {
-            return settingsXhr
-                .then(() => {
+        getTotalLoggedTimeByTitleAsync: function(title) {
+            return getSettingsAsync()
+                .then((settings) => {
                     const params = {
                         user_agent: 'jira',
-                        workspace_id: _workspaceId,
-                        user_ids: _userId,
+                        workspace_id: settings.workspaceId,
+                        user_ids: settings.userId,
                         description: title,
                         since: moment().add(-365, 'd').format('YYYY-MM-DD'),
                         until: moment().format('YYYY-MM-DD')
@@ -105,7 +114,7 @@ define([
                             type: 'GET',
                             async: true
                         })
-                        .then(function (response) {
+                        .then(function(response) {
                             return Math.round(response.total_grand / 1000);
                         });
                 });
