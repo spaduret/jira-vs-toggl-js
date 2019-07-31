58cd981c168a32bf214df5a5dad49b7366c2a820
Serghei Paduret <serghei.paduret@monsato.md>
2019-07-31 15:21:21 +0300

    fix issue when extensions installed first time

--- scripts/services/toggl.service.js
+++ scripts/services/toggl.service.js
@@ -16,41 +16,41 @@ define([
 
     let headers = {};
     const deferred = new $.Deferred();
-    const getSettingsAsync = function() {
-        if(deferred.state() != "resolved") {
-            headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic ' + btoa(settings.toggl.apiToken + ':api_token')
-            };
-
-            $
-                .ajax({
-                    url: urls.me,
-                    headers: headers,
-                    type: 'GET',
-                    async: true
-                })
-                .fail(deferred.reject)
-                .then(function(response) {
-                    const workspace = _(response.data.workspaces)
-                        .find(function(ws) {
-                            return ws.name === settings.toggl.workspace;
-                        });
-
-                    deferred.resolve({
-                        workspaceId: workspace.id,
-                        userId: response.data.id
-                    });
-                });
-        }
-
-        return deferred.promise();
-    };
 
     return {
         settings: settings.toggl,
+        getSettingsAsync: function() {
+            if(deferred.state() != "resolved") {
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic ' + btoa(settings.toggl.apiToken + ':api_token')
+                };
+
+                $
+                    .ajax({
+                        url: urls.me,
+                        headers: headers,
+                        type: 'GET',
+                        async: true
+                    })
+                    .fail(deferred.reject)
+                    .then(function(response) {
+                        const workspace = _(response.data.workspaces)
+                            .find(function(ws) {
+                                return ws.name === settings.toggl.workspace;
+                            });
+
+                        deferred.resolve({
+                            workspaceId: workspace.id,
+                            userId: response.data.id
+                        });
+                    });
+            }
+
+            return deferred.promise();
+        },
         getSummaryAsync: function(period) {
-            return getSettingsAsync()
+            return this.getSettingsAsync()
                 .then((settings) => {
                     const params = {
                         user_agent: 'jira',
@@ -96,7 +96,7 @@ define([
         },
         // get all time entries for the last 365 days
         getTotalLoggedTimeByTitleAsync: function(title) {
-            return getSettingsAsync()
+            return this.getSettingsAsync()
                 .then((settings) => {
                     const params = {
                         user_agent: 'jira',
