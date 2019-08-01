2a7ddbd3cac500e2f448a9ace181c8b3dc07b7c3
Serghei Paduret <serghei.paduret@monsato.md>
2019-08-01 15:48:10 +0300

    use 'orange' color if there are negative unsynced time

--- apps/background/background.js
+++ apps/background/background.js
@@ -1,7 +1,13 @@
 chrome.runtime.onInstalled.addListener(function () {
     require(['../../scripts/config'], function () {
-        require(['sync.service'], function (syncService) {
-            const timerId = syncService.timerSetup();
+        require(['sync.service', 'moment'], function (syncService, moment) {
+                syncService.updateUnsyncedTaskCount();
+
+                const intervalDuration = moment.duration(5, 'min');
+                // run every X minutes
+                const timerId = setInterval(function() {
+                    syncService.updateUnsyncedTaskCount();
+                }, intervalDuration.asMilliseconds());
         });
     });
 });
--- scripts/services/sync.service.js
+++ scripts/services/sync.service.js
@@ -6,10 +6,10 @@ define([
     "jira.service",
     'sync.item',
     'settings'
-], function ($,_, moment, togglService, jiraService, SyncItem, settings) {
+], function($, _, moment, togglService, jiraService, SyncItem, settings) {
     return {
         // Returns toggl history with logged time
-        getWorkLogAsync: function () {
+        getWorkLogAsync: function() {
             const period = {
                 start: moment().subtract(settings.reportingRange),
                 end: moment()
@@ -20,15 +20,15 @@ define([
             togglService
                 .getSummaryAsync(period)
                 .fail(deferred.reject)
-                .done(function (summary) {
+                .done(function(summary) {
                     const tasks = _(summary).pluck('task');
 
                     return jiraService
                         .getIssuesAsync(tasks)
-                        .then(function (issues) {
+                        .then(function(issues) {
                             _(issues)
-                                .each(function (issue) {
-                                    const togglLog = _(summary).find(function (task) {
+                                .each(function(issue) {
+                                    const togglLog = _(summary).find(function(task) {
                                         return issue.key === task.task;
                                     });
 
@@ -38,7 +38,7 @@ define([
                                     $
                                         .when(issueWorklogTask, togglLoggedTimeTask)
                                         .fail(deferred.reject)
-                                        .done(function (issueWorklog, totalTime) {
+                                        .done(function(issueWorklog, totalTime) {
 
                                             const jiraTime = Math.round(issueWorklog / 60);
                                             const togglTime = Math.round(totalTime / 60);
@@ -46,7 +46,7 @@ define([
 
                                             taskLog.push(item);
 
-                                            if (taskLog.length === issues.length) {
+                                            if(taskLog.length === issues.length) {
                                                 deferred.resolve(taskLog);
                                             }
                                         });
@@ -56,54 +56,46 @@ define([
 
             return deferred.promise();
         },
-        syncAsync: function (itemToSync) {
-            if (!(itemToSync.unsynced > 0))
+        syncAsync: function(itemToSync) {
+            if(!(itemToSync.unsynced > 0))
                 throw new Error('Can log only positive time, Bad time value: ' + itemToSync.unsynced);
 
-            if (!itemToSync.logDate.isValid())
+            if(!itemToSync.logDate.isValid())
                 throw new Error('Log date is required');
 
             return jiraService.logWorkAsync(itemToSync);
         },
-        timerSetup: function () {
-            let view = this;
-            view.updateUnsyncedTaskCount();
-
-            // run every X minutes
-            const timerId = setInterval(function () {
-                view.updateUnsyncedTaskCount();
-            }, 1 * 60 * 1000);
-
-            return timerId;
-        },
-        updateUnsyncedTaskCount: function (tasksCount) {
+        updateUnsyncedTaskCount: function(tasksCount) {
             const view = this;
 
-            if (isNaN(tasksCount)) {
+            if(isNaN(tasksCount)) {
                 view
                     .getWorkLogAsync()
-                    .fail(function(xhr, status, error){
+                    .fail(function(xhr, status, error) {
                         chrome.browserAction.setBadgeText({text: '*' + xhr.status});
                         chrome.browserAction.setBadgeBackgroundColor({color: "black"});
                     })
-                    .done(function (log) {
-                        const count = _(log).filter(function (w) {
-                            return w.unsynced > settings.timeToIgnoreMinutes;
-                        }).length;
+                    .done(function(log) {
+                        const count = _(log).filter((w) => w.unsynced > settings.timeToIgnoreMinutes).length;
 
-                        view.setBadgeText(count);
+                        const negativeTimeCount = _(log).some((w) => w.unsynced < 0);
+                        let badgeColor = null;
+                        if(negativeTimeCount > 0)
+                            badgeColor = "orange";
+
+                        view.setBadgeText(count, badgeColor);
                     });
             } else {
                 view.setBadgeText(tasksCount);
             }
         },
-        setBadgeText: function(count){
-            if (count > 0) {
+        setBadgeText: function(count, badgeColor) {
+            if(count > 0) {
                 chrome.browserAction.setBadgeText({text: count.toString()});
-                chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+                chrome.browserAction.setBadgeBackgroundColor({color: badgeColor || "red"});
             } else {
                 chrome.browserAction.setBadgeText({text: '0'});
-                chrome.browserAction.setBadgeBackgroundColor({color: "green"});
+                chrome.browserAction.setBadgeBackgroundColor({color: badgeColor || "green"});
             }
 
             console.log('setBadgeText: ' + count);
