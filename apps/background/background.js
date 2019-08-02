b70478f5040ef3af20ff8caf9de46c0b8db044fc
Serghei Paduret <serghei.paduret@monsato.md>
2019-08-02 15:34:10 +0300

    fix issue with refreshing interval

--- apps/background/background.js
+++ apps/background/background.js
@@ -1,11 +1,12 @@
-chrome.runtime.onInstalled.addListener(function () {
-    require(['../../scripts/config'], function () {
-        require(['sync.service', 'moment'], function (syncService, moment) {
-                syncService.updateUnsyncedTaskCount();
+chrome.runtime.onInstalled.addListener(function() {
+    require(['../../scripts/config'], function() {
+        require(['sync.service', 'moment'], function(syncService, moment) {
+            syncService.updateUnsyncedTaskCount();
 
-                const intervalDuration = moment.duration(5, 'min');
-                // run every X minutes
-                const timerId = setInterval(function() {
+            const intervalDuration = moment.duration(5, 'minutes');
+            // run every X minutes
+            if(!window.jvt_timer_id)
+                window.jvt_timer_id = setInterval(function() {
                     syncService.updateUnsyncedTaskCount();
                 }, intervalDuration.asMilliseconds());
         });
